fe066d5b27b4bc837d8814c50388d0b479610c6e
Alicen Henning <2127787@students.wits.ac.za>
2023-08-11 11:02:29 +0200

    adjustments to plot

--- scripts/00_seurat_full_workflow/alpha_full_seurat.R
+++ scripts/00_seurat_full_workflow/alpha_full_seurat.R
@@ -198,8 +198,105 @@ Seurat::DimPlot(
   group.by = 'seurat_clusters',
   pt.size = 1,
   label = FALSE,
-  cols = palette.a
+  # cols = palette.a
 )
+
+# ///
+  
+# Alternatively, using ggplot :   
+
+# Extract UMAP coordinates and cluster information
+alpha.umap.coords <- as.data.frame(alpha@reductions$umap@cell.embeddings)
+clusters <- alpha$seurat_clusters
+
+# Create a dataframe for ggplot
+alpha.df <- data.frame(
+  x = alpha.umap.coords$UMAP_1,
+  y = alpha.umap.coords$UMAP_2,
+  seurat_clusters = factor(clusters)
+)
+
+# Define color palette
+palette.a <- RColorBrewer::brewer.pal(11, "Paired")
+
+# Create the ggplot plot
+ggplot(alpha.df, aes(x, y, colour = seurat_clusters)) +
+  geom_point(size = 1) +
+  scale_colour_manual(values = palette.a) +
+  labs(#title = "IFN alpha",
+       x = "UMAP 1",  # Rename x-axis label
+       y = "UMAP 2",
+       color = "")  + 
+  theme_minimal() + 
+  theme(#panel.background = element_rect(fill = "lightgrey"),  # Set background color
+       panel.grid.minor = element_blank(),  # Remove minor gridlines
+      panel.grid.major = element_blank(),
+        axis.text = element_text(size = 12),  # Increase axis label size
+        axis.title = element_text(size = 14), 
+        plot.margin = margin(1.5, 0.5, 0.5, 0.5, "cm"),
+        panel.border = element_rect(color = "black", fill = NA),
+        #legend.background = element_rect(color = "black", fill = "white"),
+        legend.position = "right", 
+        legend.title = element_text(size =  14),
+        legend.text = element_text(size = 14),
+        plot.title = element_text(size = 18, face = "bold", hjust = 0.5, margin = margin(1, 0, 0, 0))) + 
+  guides(color = guide_legend(
+    override.aes = list(
+      #hape = rep(22, length(palette.a)),  # Use squares (blocks)
+      fill = palette.a, 
+      size = 3.5),  # Color the squares with the same palette
+      key_height = unit(1, "npc"),  # Spread the legend dots across the vertical length
+      key_width = unit(4, "cm"),   # Adjust the width of the legend blocks
+      title.theme = element_text(hjust = 0.5),  # Center the legend title
+    label.position = "right",
+    label.hjust = 1
+  ))
+
+# panel.background = element_rect(fill = "grey"),
+
+# working on facet plot 
+
+# custom_color <- function(x, cluster) {
+#   ifelse(x == cluster, palette.a[cluster], "grey")
+# }
+# 
+# # Create the facet plot
+# ggplot(alpha.df, aes(x, y)) +
+#   geom_point(aes(colour = seurat_clusters), size = 1) +
+#   scale_colour_manual(values = palette.a) +
+#   labs(title = "IFN alpha",
+#        x = "UMAP 1",
+#        y = "UMAP 2",
+#        color = "Seurat Clusters") +
+#   theme_minimal() +
+#   theme(
+#     panel.grid.major = element_blank(),
+#     axis.text = element_text(size = 18),
+#     axis.title = element_text(size = 18),
+#     plot.margin = margin(0.5, 0.5, 0.5, 0.5, "cm"),
+#     panel.border = element_rect(color = "black", fill = NA),
+#     legend.background = element_rect(fill = "white"),  # Remove legend box
+#     legend.position = "right",
+#     legend.title = element_text(size = 16),
+#     legend.text = element_text(size = 12),
+#     plot.title = element_text(size = 18, face = "bold", hjust = 0.5)
+#   ) +
+#   guides(color = guide_legend(
+#     override.aes = list(
+#       shape = rep(22, length(palette.a)),  # Use squares (blocks)
+#       fill = palette.a  # Color the squares with the same palette
+#     ),
+#     key_height = unit(2, "cm"),  # Adjust the height of the legend blocks
+#     key_width = unit(2, "cm"),   # Adjust the width of the legend blocks,
+#     title.theme = element_text(hjust = 0.5)  # Center the legend title
+#   )) +
+#   facet_wrap(~ ifelse(seurat_clusters %in% c(10, 11), "10-11", as.character(seurat_clusters)), ncol = 5) +
+#   scale_colour_manual(values = sapply(1:11, custom_color, cluster = 1))
+
+
+
+
+
 
 
 ##### Finding differentially expressed features #####
