9894da5c8b7e65dd314134081b697711a8c3cd35
Alicen Henning <2127787@students.wits.ac.za>
2023-08-15 12:59:29 +0200

    Script for converting ENSEMBL gene IDs to HGNC gene names

--- scripts/00_useful/ENSEMBL_2_HGNC.R
+++ scripts/00_useful/ENSEMBL_2_HGNC.R
@@ -1,6 +1,6 @@
 # This script is to convert ENSEMBL gene names to standard HGNC
 
-# First need to install a mapping between Ensemble gene IDs and HGNC gene names 
+##### Install a mapping between Ensemble gene IDs and HGNC gene names ####
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("EnsDb.Hsapiens.v86")
@@ -20,4 +20,32 @@ gene_data <- select(edb, keys = keys(edb, keytype = "GENEID"),
 # change the names of the columns for clarity : 
 
 colnames(gene_data) <- c("ENSEMBL_Gene_ID", "HGNC_Name")
-View(gene_data)
+View(gene_data) # view the wonders 
+
+
+# save the data frame as a csv 
+
+write.csv(gene_data, file = "honours/gene_data.csv", row.names = FALSE)
+
+
+
+##### Create a new feature metadata slot for seurat object (for HGNC) ####
+
+# Convert your mapping data to a named vector : 
+
+ensembl_to_hgnc_map <- setNames(gene_data$HGNC_Name, gene_data$ENSEMBL_Gene_ID)
+
+# Update the feature metadata slot in your Seurat object :
+
+untreated@assays[["RNA"]]@meta.features$gene <- ensembl_to_hgnc_map[rownames(untreated@assays[["RNA"]]@meta.features)]
+
+# Update gene names in the count matrix based on the mapping : 
+
+rownames(untreated@assays[["RNA"]]@counts) <- untreated@assays[["RNA"]]@meta.features$gene
+
+View(untreated) # look 
+
+
+##### Saving new seurat object #####
+
+saveRDS(untreated, file= "honours/untreated/untreated.rds")
