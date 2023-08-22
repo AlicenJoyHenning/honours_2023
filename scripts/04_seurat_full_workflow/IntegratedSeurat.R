35d9310e517337e73218a58d35369b7b58ae3e9a
Alicen Henning <2127787@students.wits.ac.za>
2023-08-22 08:30:55 +0200

    Clustering images

--- scripts/04_seurat_full_workflow/IntegratedSeurat.R
+++ scripts/04_seurat_full_workflow/IntegratedSeurat.R
@@ -10,9 +10,14 @@ BiocManager::install('multtest')
 BiocManager::install('metap')
 BiocManager::install('xlsx')
 BiocManager::install('pheatmap')
+BiocManager::install('XLConnect')
+BiocManager::install('writexl')
+
 
 library(dplyr)
-library()
+library(writexl)
+library(openxlsx)
+library(XLConnect)
 library(ggplot2)
 library(grid)
 library(Seurat)
@@ -29,7 +34,7 @@ library(openxlsx)
 library(cowplot)
 library(readxl)
 library(xlsx)
-
+library(wri)
 
 # Load datasets: alpha, lambda, and untreated
 
@@ -39,8 +44,8 @@ library(xlsx)
 # lambda <- CreateSeuratObject(counts=lambda, project='ifnlambda', min.cells=3, min.features=200)
 
 # untreated object first requires the matrix to be made : 
-UntreatedMatrix <- ReadMtx("honours/work/DarisiaIndex/untreatedDarisiaIndex/seurat_matrix/matrix.mtx.gz", "honours/work/DarisiaIndex/untreatedDarisiaIndex/seurat_matrix/barcodes.tsv.gz", "honours/work/DarisiaIndex/untreatedDarisiaIndex/seurat_matrix/AdjustedFeatures.tsv.gz", )
-untreated <- CreateSeuratObject(matrix, project="untreated", min.cells=3, min.features=200)
+UntreatedMatrix <- ReadMtx("honours/work/DarisiaIndex/untreatedDarisiaIndex/seurat_matrix/matrix.mtx.gz", "honours/work/DarisiaIndex/untreatedDarisiaIndex/seurat_matrix/barcodes.tsv.gz", "honours/work/DarisiaIndex/untreatedDarisiaIndex/seurat_matrix/features.tsv.gz")
+untreated <- CreateSeuratObject(UntreatedMatrix, project="untreated", min.cells=3, min.features=200)
 
 AlphaMatrix <- ReadMtx("honours/work/DarisiaIndex/ifnalphaDarisiaIndex/seurat_matrix/matrix.mtx.gz","honours/work/DarisiaIndex/ifnalphaDarisiaIndex/seurat_matrix/barcodes.tsv.gz", "honours/work/DarisiaIndex/ifnalphaDarisiaIndex/seurat_matrix/AdjustedFeatures.tsv.gz", skip.feature = 1)
 alpha <- CreateSeuratObject(AlphaMatrix, project="alpha", min.cells=3, min.features=200)
@@ -95,7 +100,7 @@ untreated <- ScaleData(untreated, features = all.untreated.genes)
 
 ##### Prepare datasets for integration #####
 
-TreatmentList <- list(alpha, lambda)# untreated) # Create a list of Seurat objects
+TreatmentList <- list(alpha, lambda, untreated) # Create a list of Seurat objects
 
 TreatmentFeatures <- SelectIntegrationFeatures(object.list = TreatmentList) # Select features that are repeatedly variable across datasets for integration : 
 # electing features (genes) that are consistently variable across multiple datasets for the purpose of integrating those datasets into a single analysis
@@ -111,7 +116,7 @@ TreatmentFeatures <- SelectIntegrationFeatures(object.list = TreatmentList) # Se
 
 anchors <- FindIntegrationAnchors(
   object.list = TreatmentList, 
-  reference = 1,
+  reference = 3,
   anchor.features = TreatmentFeatures
 )
 # Found 13669 anchors, retained 
@@ -147,29 +152,139 @@ p1 + p2
 #### Identify Conserved cell type markers #####
 # performing differential expression after integration :
 DefaultAssay(treatment) <- "RNA"
+
+ClusterPath <- "honours/results/IntegratedMarkers/SeuratClusters.xlsx"
+
 Cluster0Markers <- FindConservedMarkers(treatment, ident.1= 0, grouping.var = "stim", verbose = FALSE)
+Cluster0Markers <- subset(Cluster0Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster0Markers <- subset(Cluster0Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C0Rows <- rownames(Cluster0Markers)
+Cluster0Markers <- cbind(RowNames = C0Rows, Cluster0Markers)
+write.xlsx(Cluster0Markers, ClusterPath, sheetName = "Cluster0")
+dim(Cluster0Markers) # Before adjustments 1755 > 1217 > 57
+
+
 Cluster1Markers <- FindConservedMarkers(treatment, ident.1= 1, grouping.var = "stim", verbose = FALSE)
+Cluster1Markers <- subset(Cluster1Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster1Markers <- subset(Cluster1Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C1Rows <- rownames(Cluster1Markers)
+Cluster1Markers <- cbind(RowNames = C1Rows, Cluster1Markers)
+openxlsx::write.xlsx(Cluster1Markers, ClusterPath, sheetName = "Cluster1", append = TRUE)
+dim(Cluster1Markers) # Before adjustments : 1841 > 1368 > 47
+
 Cluster2Markers <- FindConservedMarkers(treatment, ident.1= 2, grouping.var = "stim", verbose = FALSE)
+Cluster2Markers <- subset(Cluster2Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster2Markers <- subset(Cluster2Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C2Rows <- rownames(Cluster2Markers)
+Cluster2Markers <- cbind(RowNames = C2Rows, Cluster2Markers)
+dim(Cluster2Markers) # Before adjustments : 1730 > 99
+
 Cluster3Markers <- FindConservedMarkers(treatment, ident.1= 3, grouping.var = "stim", verbose = FALSE)
+Cluster3Markers <- subset(Cluster3Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster3Markers <- subset(Cluster3Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C3Rows <- rownames(Cluster3Markers)
+Cluster3Markers <- cbind(RowNames = C3Rows, Cluster3Markers)
+dim(Cluster3Markers) # Before adjustments : 1637 > 1043 > 26
+
+Cluster4Markers <- FindConservedMarkers(treatment, ident.1= 4, grouping.var = "stim", verbose = FALSE)
+Cluster4Markers <- subset(Cluster4Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster4Markers <- subset(Cluster4Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C4Rows <- rownames(Cluster4Markers)
+Cluster4Markers <- cbind(RowNames = C4Rows, Cluster4Markers)
+dim(Cluster4Markers) # 1699  > 1237 > 46
+
+Cluster5Markers <- FindConservedMarkers(treatment, ident.1= 5, grouping.var = "stim", verbose = FALSE)
+Cluster5Markers <- subset(Cluster5Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster5Markers <- subset(Cluster5Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C5Rows <- rownames(Cluster5Markers)
+Cluster5Markers <- cbind(RowNames = C5Rows, Cluster5Markers)
+dim(Cluster5Markers) # 1525 > 821 > 57
+
+Cluster6Markers <- FindConservedMarkers(treatment, ident.1= 6, grouping.var = "stim", verbose = FALSE)
+#
+Cluster6Markers <- subset(Cluster6Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster6Markers <- subset(Cluster6Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C6Rows <- rownames(Cluster6Markers)
+Cluster6Markers <- cbind(RowNames = C6Rows, Cluster6Markers)
+dim(Cluster6Markers)
 
 
+Cluster7Markers <- FindConservedMarkers(treatment, ident.1= 7, grouping.var = "stim", verbose = FALSE)
+Cluster7Markers <- subset(Cluster7Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster7Markers <- subset(Cluster7Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C7Rows <- rownames(Cluster7Markers)
+Cluster7Markers <- cbind(RowNames = C7Rows, Cluster7Markers)
+dim(Cluster7Markers)
 
-# output = list of genes 
+Cluster8Markers <- FindConservedMarkers(treatment, ident.1= 8, grouping.var = "stim", verbose = FALSE)
+Cluster8Markers <- subset(Cluster8Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster8Markers <- subset(Cluster8Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C8Rows <- rownames(Cluster8Markers)
+Cluster8Markers <- cbind(RowNames = C8Rows, Cluster8Markers)
+dim(Cluster8Markers)
+
+Cluster9Markers <- FindConservedMarkers(treatment, ident.1= 9, grouping.var = "stim", verbose = FALSE)
+Cluster9Markers <- subset(Cluster9Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster9Markers <- subset(Cluster9Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C9Rows <- rownames(Cluster9Markers)
+Cluster9Markers <- cbind(RowNames = C9Rows, Cluster9Markers)
+dim(Cluster9Markers)
+
+Cluster10Markers <- FindConservedMarkers(treatment, ident.1= 10, grouping.var = "stim", verbose = FALSE)
+Cluster10Markers <- subset(Cluster10Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster10Markers <- subset(Cluster10Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C10Rows <- rownames(Cluster10Markers)
+Cluster10Markers <- cbind(RowNames = C10Rows, Cluster10Markers)
+dim(Cluster10Markers)
+
+Cluster11Markers <- FindConservedMarkers(treatment, ident.1= 11, grouping.var = "stim", verbose = FALSE)
+Cluster11Markers <- subset(Cluster11Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster11Markers <- subset(Cluster11Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C11Rows <- rownames(Cluster11Markers)
+Cluster11Markers <- cbind(RowNames = C11Rows, Cluster11Markers)
+dim(Cluster11Markers)
+
+Cluster12Markers <- FindConservedMarkers(treatment, ident.1= 12, grouping.var = "stim", verbose = FALSE)
+Cluster12Markers <- subset(Cluster12Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster12Markers <- subset(Cluster12Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C12Rows <- rownames(Cluster12Markers)
+Cluster12Markers <- cbind(RowNames = C12Rows, Cluster12Markers)
+dim(Cluster12Markers)
+
+Cluster13Markers <- FindConservedMarkers(treatment, ident.1= 13, grouping.var = "stim", verbose = FALSE)
+Cluster13Markers <- subset(Cluster13Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster13Markers <- subset(Cluster13Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C13Rows <- rownames(Cluster13Markers)
+Cluster13Markers <- cbind(RowNames = C13Rows, Cluster13Markers)
+dim(Cluster13Markers)
+
+Cluster14Markers <- FindConservedMarkers(treatment, ident.1= 14, grouping.var = "stim", verbose = FALSE)
+Cluster14Markers <- subset(Cluster14Markers, subset = lambda_p_val_adj < 0.05 & alpha_p_val_adj < 0.05 & untreated_p_val_adj < 0.05) 
+Cluster14Markers <- subset(Cluster14Markers, subset = lambda_avg_log2FC > 1 & alpha_avg_log2FC > 1 & untreated_avg_log2FC > 1) 
+C14Rows <- rownames(Cluster14Markers)
+Cluster14Markers <- cbind(RowNames = C14Rows, Cluster14Markers)
+dim(Cluster14Markers)
+
+
+# Saving the data to an excel sheet to annotate genes : 
+clusters <- list('Cluster0' = Cluster0Markers, 'Cluster1' = Cluster1Markers,'Cluster2' = Cluster2Markers, 'Cluster3' = Cluster3Markers,'Cluster4' = Cluster4Markers, 'Cluster5' = Cluster5Markers, 'Cluster6' = Cluster6Markers, 'Cluster7' = Cluster7Markers, 'Cluster8' = Cluster8Markers, 'Cluster9' = Cluster9Markers, 'Cluster10' = Cluster10Markers, 'Cluster11' = Cluster11Markers,'Cluster12' = Cluster12Markers,'Cluster13' = Cluster13Markers,'Cluster14' = Cluster14Markers)
+openxlsx::write.xlsx(clusters, file = "honours/results/IntegratedMarkers/SeuratMarkers.xlsx")
+
 
 # check for validity of markers within this list of genes : 
 # using the markers identified, a set of Feature plots will br made fo each gene (marker) to see if they acurately describe clusters: 
-Cluster0FP <-  FeaturePlot(treatment, features = c("CCR7", "CD7"), min.cutoff = "q9")
+Cluster0FP <-  FeaturePlot(treatment, features = c("CCR7", "CD7"))
 Cluster1FP <-  FeaturePlot(treatment, features = c("CCL2"))
-Cluster2FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
-Cluster3FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
-Cluster4FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
-Cluster5FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
-Cluster6FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
-Cluster7FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
-Cluster8FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
-Cluster9FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
-Cluster10FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
-Cluster11FP <-  FeaturePlot(treatment, features = c(""), min.cutoff = "q9")
+Cluster2FP <-  FeaturePlot(treatment, features = c(""))
+Cluster3FP <-  FeaturePlot(treatment, features = c(""))
+Cluster4FP <-  FeaturePlot(treatment, features = c(""))
+Cluster5FP <-  FeaturePlot(treatment, features = c(""))
+Cluster6FP <-  FeaturePlot(treatment, features = c(""))
+Cluster7FP <-  FeaturePlot(treatment, features = c(""))
+Cluster8FP <-  FeaturePlot(treatment, features = c(""))
+Cluster9FP <-  FeaturePlot(treatment, features = c(""))
+Cluster10FP <-  FeaturePlot(treatment, features = c(""))
+Cluster11FP <-  FeaturePlot(treatment, features = c(""))
 
 VlnPlot(treatment, features = "FCGR3A")
 
@@ -266,7 +381,7 @@ predictions <- SingleR(GetAssayData(treatment, assay = "RNA", slot = "data"),
                        ref = reference,
                        labels = reference$label.fine)
 Idents(treatment)
-write.csv(predictions$labels,"honours/results/SRPredictionsBEDFine")
+write.csv(predictions$labels,"honours/results/PredictionsBEDFine.txt")
 plotScoreHeatmap(predictions)
 
 SingleRBPETreatment <- RenameIdents(treatment,
