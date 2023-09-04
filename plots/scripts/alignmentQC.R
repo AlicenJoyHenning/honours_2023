6e6c54ff773f4053782a04a48c6dfa6d0810703e
= <=>
2023-09-05 00:07:05 +0200

    script for kallisto alignment QC plots

---
+++ plots/scripts/alignmentQC.R
@@ -0,0 +1,31 @@
+# Alignment Quality 
+
+library(ggplot2)
+Datasets <- c(rep("alpha" , 2), rep("lambda", 2), rep("untreated", 2)) 
+Metrics <- rep(c("Alignment Rate" , "Percentage Whitelist" ),3)
+Percentage <- c(56.6, 95.6, 55.3, 95.7, 54.1, 96.1)
+
+data <- data.frame(group,metrics,values)
+
+# Grouped
+ggplot(data, aes(fill=Metrics, y=Percentage, x=Datasets)) + 
+  geom_bar(position="dodge", stat="identity") +
+  geom_text(aes(label = Percentage),  position = position_dodge(width = 0.9), 
+            vjust = -0.5, size = 3.5) +
+  scale_fill_manual(values = c("grey", "#6ab5ba")) +
+ # NO LITERATURE TO SUPPORT geom_hline(yintercept =50, linetype = "dotted", color = "black") + 
+ # geom_hline(yintercept = 98, linetype = "dotted", color = "darkblue") +
+  coord_flip()
+
+
+ggplot(data, aes(fill = Metrics, y = Percentage, x = Datasets)) + 
+  geom_bar(position = "dodge", stat = "identity") +
+  scale_fill_manual(values = c("grey", "#6ab5ba")) +
+  coord_flip() +
+  
+  # Add labels inside the bars at the top
+  geom_text(aes(label = Percentage, y = Percentage + 5),  # Adjust the 'y' position
+            position = position_dodge(width = 0.9), 
+            size = 3.5, hjust = 0.5) +  # Center the labels horizontally
+  
+  labs(y = "Percentage") 
