e06d01374794f7b9ca181c0d9d18a21af4f1c5c3
= <2127787@students.wits.ac.za>
2023-09-26 01:59:26 +0200

    Improve output appearance of for loop

--- scripts/04_Seurat/4_2_PBMC_proportions.R
+++ scripts/04_Seurat/4_2_PBMC_proportions.R
@@ -1,15 +1,15 @@
 # Quantifying PBMC populations across treatment 
 
-# [1] Load annotated dataset : 
+##### [1] Load annotated dataset : ####
 TreatmentAnnotated <- readRDS("honours/results/FinalIndex/TreatmentAnnotated.rds")
 DefaultAssay(TreatmentAnnotated)
 DefaultAssay(TreatmentAnnotated) <- "RNA"
 
-# [2] View and store PBMC population names : 
+##### [2] View and store PBMC population names : ####
 TreatmentAnnotated$celltype <- Idents(TreatmentAnnotated)
 levels(TreatmentAnnotated)
 
-# [3] Quantify 
+##### [3] Quantify using for loop : ####
 # Define the list of cell types
 cell_types <- c(
   "monocytes", "CD4+_helper", "neutrophils", "T", "naive_CD8+ T",
@@ -23,19 +23,22 @@ results <- list()
 # Loop through each cell type
 for (cell_type in cell_types) {
   # Create a subset for each treatment and cell type combination
+  T_subset <- subset(TreatmentAnnotated, subset = celltype == cell_type)
   A_subset <- subset(TreatmentAnnotated, subset = celltype == cell_type & treatment == "alpha")
   L_subset <- subset(TreatmentAnnotated, subset = celltype == cell_type & treatment == "lambda")
   U_subset <- subset(TreatmentAnnotated, subset = celltype == cell_type & treatment == "untreated")
   
   # Quantify the number of cells in each subset
+  num_cells_T <- dim(T_subset)[2]
   num_cells_A <- dim(A_subset)[2]
   num_cells_L <- dim(L_subset)[2]
   num_cells_U <- dim(U_subset)[2]
   
   # Store the results in a list
-  cat("\n Cell Type:", cell_type, "alpha", num_cells_A)
-  cat("\n Cell Type:", cell_type, "lambda", num_cells_L)
-  cat("\n Cell Type:", cell_type, "lambda", num_cells_U)
+  cat("\n Cell Type | ", cell_type, "\n", "Total:", num_cells_A)
+  cat("\n Alpha :", num_cells_A)
+  cat("\n Lambda :", num_cells_L)
+  cat("\n Untreated :", num_cells_U, "\n")
 }
 
 
